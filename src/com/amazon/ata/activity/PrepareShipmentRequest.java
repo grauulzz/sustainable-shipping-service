5b318c5733477eca916c0c60e9067280247e804d
Will Reynolds <billiamreynolds@gmail.com>
2021-11-29 17:26:44 -0600

    checkstyleMain cleanup1

--- src/com/amazon/ata/activity/PrepareShipmentRequest.java
+++ src/com/amazon/ata/activity/PrepareShipmentRequest.java
@@ -4,50 +4,64 @@ import java.math.BigDecimal;
 import java.util.Objects;
 
 public class PrepareShipmentRequest {
-
+    
     /**
-    * Represents a request sent to app for packaging information.
-    */
-
+     * Represents a request sent to app for packaging information.
+     */
+    
     /**
-    * The item's unique identifier - asing - (Amazon Standard Identification Number - 10 alphanumeric characters).
-    */
+     * The item's unique identifier - asing - (Amazon Standard Identification Number - 10 alphanumeric characters).
+     */
     private String asin;
     /**
-    * A description of the item.
-    */
+     * A description of the item.
+     */
     private String description;
-
+    
     /**
-    * The length of the item, in centimeters.
-    */
+     * The length of the item, in centimeters.
+     */
     private BigDecimal length;
-
+    
     /**
-    * The shortest dimension of the item, in centimeters.
-    */
+     * The shortest dimension of the item, in centimeters.
+     */
     private BigDecimal width;
-
+    
     /**
-    * The longest dimension of the item, in centimeters.
-    */
+     * The longest dimension of the item, in centimeters.
+     */
     private BigDecimal height;
-
+    
     /**
-    * The unique identifier code for a fulfillment center 4-chars where first 3 are airport code for nearest city.
-    */
+     * The unique identifier code for a fulfillment center 4-chars where first 3 are airport code for nearest city.
+     */
     private String fcCode;
-
+    
+    
+    /**
+     * Instantiates a new Prepare shipment request.
+     */
     public PrepareShipmentRequest() {
-        this.asin   = "0000000000";
+        this.asin = "0000000000";
         this.description = "Frank Test Default Item - should not ever be needed - only used for testing";
         this.length = new BigDecimal(0);
-        this.width  = new BigDecimal(0);
+        this.width = new BigDecimal(0);
         this.height = new BigDecimal(0);
         this.fcCode = "IND1";
     }
-
-
+    
+    
+    /**
+     * Instantiates a new Prepare shipment request.
+     *
+     * @param asin        the asin
+     * @param description the description
+     * @param length      the length
+     * @param width       the width
+     * @param height      the height
+     * @param fcCode      the fc code
+     */
     public PrepareShipmentRequest(String asin, String description, BigDecimal length, BigDecimal width,
                                   BigDecimal height, String fcCode) {
         this.asin = asin;
@@ -57,7 +71,12 @@ public class PrepareShipmentRequest {
         this.height = height;
         this.fcCode = fcCode;
     }
-
+    
+    /**
+     * Instantiates a new Prepare shipment request.
+     *
+     * @param builder the builder
+     */
     public PrepareShipmentRequest(Builder builder) {
         this.asin = builder.asin;
         this.description = builder.description;
@@ -66,55 +85,55 @@ public class PrepareShipmentRequest {
         this.height = builder.height;
         this.fcCode = builder.fcCode;
     }
-
+    
     public String getAsin() {
         return asin;
     }
-
+    
     public void setAsin(String asin) {
         this.asin = asin;
     }
-
+    
     public String getDescription() {
         return description;
     }
-
+    
     public void setDescription(String description) {
         this.description = description;
     }
-
+    
     public BigDecimal getLength() {
         return length;
     }
-
+    
     public void setLength(BigDecimal length) {
         this.length = length;
     }
-
+    
     public BigDecimal getWidth() {
         return width;
     }
-
+    
     public void setWidth(BigDecimal width) {
         this.width = width;
     }
-
+    
     public BigDecimal getHeight() {
         return height;
     }
-
+    
     public void setHeight(BigDecimal height) {
         this.height = height;
     }
-
+    
     public String getFcCode() {
         return fcCode;
     }
-
+    
     public void setFcCode(String fcCode) {
         this.fcCode = fcCode;
     }
-
+    
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -125,17 +144,17 @@ public class PrepareShipmentRequest {
         }
         PrepareShipmentRequest that = (PrepareShipmentRequest) o;
         return getAsin().equals(that.getAsin()) && getDescription().equals(that.getDescription()) &&
-                                                getLength().equals(that.getLength()) &&
-                                                getWidth().equals(that.getWidth()) &&
-                                                getHeight().equals(that.getHeight()) &&
-                                                getFcCode().equals(that.getFcCode());
+                getLength().equals(that.getLength()) &&
+                getWidth().equals(that.getWidth()) &&
+                getHeight().equals(that.getHeight()) &&
+                getFcCode().equals(that.getFcCode());
     }
-
+    
     @Override
     public int hashCode() {
         return Objects.hash(getAsin(), getDescription(), getLength(), getWidth(), getHeight(), getFcCode());
     }
-
+    
     @Override
     public String toString() {
         return "PrepareShipmentRequest{" +
@@ -147,7 +166,7 @@ public class PrepareShipmentRequest {
                 ", fcCode='" + fcCode + '\'' +
                 '}';
     }
-
+    
     /**
      * Returns a new PrepareShipmentActivity.Builder object for constructing an Item.
      *
@@ -156,10 +175,10 @@ public class PrepareShipmentRequest {
     public static Builder builder() {
         return new Builder();
     }
-
+    
     /**
-    * {@code} Builder builder static inner class.
-    */
+     * {@code} Builder builder static inner class.
+     */
     public static final class Builder {
         private String asin;
         private String description;
@@ -167,84 +186,85 @@ public class PrepareShipmentRequest {
         private BigDecimal width;
         private BigDecimal height;
         private String fcCode;
-
+        
         private Builder() {
         }
-
+        
         /**
-        * Sets the {@code asin} and returns a reference to this Builder so that the methods can be chained together.
-        *
-        * @param asinToUse the {@code asin} to set
-        * @return a reference to this Builder
-        */
+         * Sets the {@code asin} and returns a reference to this Builder so that the methods can be chained together.
+         *
+         * @param asinToUse the {@code asin} to set
+         * @return a reference to this Builder
+         */
         public Builder withItemAsin(String asinToUse) {
             this.asin = asinToUse;
             return this;
         }
-
+        
         /**
-        * Sets the {@code description} and returns a reference to this Builder so that the methods can be chained
-        * together.
-        *
-        * @param descriptionToUse the {@code description} to set
-        * @return a reference to this Builder
-        */
+         * Sets the {@code description} and returns a reference to this Builder so that the methods can be chained
+         * together.
+         *
+         * @param descriptionToUse the {@code description} to set
+         * @return a reference to this Builder
+         */
         public Builder withItemDescription(String descriptionToUse) {
             this.description = descriptionToUse;
             return this;
         }
-
+        
         /**
-        * Sets the {@code length} and returns a reference to this Builder so that the methods can be chained together.
-        *
-        * @param lengthToUse the {@code length} to set
-        * @return a reference to this Builder
-        */
+         * Sets the {@code length} and returns a reference to this Builder so that the methods can be chained together.
+         *
+         * @param lengthToUse the {@code length} to set
+         * @return a reference to this Builder
+         */
         public Builder withItemLength(String lengthToUse) {
             this.length = new BigDecimal(lengthToUse);
             return this;
         }
-
+        
         /**
-        * Sets the {@code width} and returns a reference to this Builder so that the methods can be chained together.
-        *
-        * @param widthToUse the {@code width} to set
-        * @return a reference to this Builder
-        */
+         * Sets the {@code width} and returns a reference to this Builder so that the methods can be chained together.
+         *
+         * @param widthToUse the {@code width} to set
+         * @return a reference to this Builder
+         */
         public Builder withItemWidth(String widthToUse) {
             this.width = new BigDecimal(widthToUse);
             return this;
         }
-
+        
         /**
-        * Sets the {@code height} and returns a reference to this Builder so that the methods can be chained together.
-        *
-        * @param heightToUse the {@code height} to set
-        * @return a reference to this Builder
-        */
+         * Sets the {@code height} and returns a reference to this Builder so that the methods can be chained together.
+         *
+         * @param heightToUse the {@code height} to set
+         * @return a reference to this Builder
+         */
         public Builder withItemHeight(String heightToUse) {
             this.height = new BigDecimal(heightToUse);
             return this;
         }
+        
         /**
-        * Sets the {@code height} and returns a reference to this Builder so that the methods can be chained together.
-        *
-        * @param fcCode the {@code fcCode} to set
-        * @return a reference to this Builder
-        */
+         * Sets the {@code height} and returns a reference to this Builder so that the methods can be chained together.
+         *
+         * @param fcCode the {@code fcCode} to set
+         * @return a reference to this Builder
+         */
         public Builder withFcCode(String fcCode) {
             this.fcCode = fcCode;
             return this;
         }
-
+        
         /**
-        * Returns a {@code Item} built from the parameters previously set.
-        *
-        * @return a {@code Item} built with parameters of this {@code Item.Builder}
-        */
+         * Returns a {@code Item} built from the parameters previously set.
+         *
+         * @return a {@code Item} built with parameters of this {@code Item.Builder}
+         */
         public PrepareShipmentRequest build() {
-        return new PrepareShipmentRequest(this);
+            return new PrepareShipmentRequest(this);
         }
     }
-
+    
 }
--- src/com/amazon/ata/activity/PrepareShipmentResponse.java
+++ src/com/amazon/ata/activity/PrepareShipmentResponse.java
@@ -11,12 +11,22 @@ public class PrepareShipmentResponse {
     private Packaging packaging;
     private FulfillmentCenter fulfillmentCenter;
 
+    /**
+     * Instantiates a new Prepare shipment response.
+     */
     public PrepareShipmentResponse() {
         this.item              = null;
         this.packaging         = null;
         this.fulfillmentCenter = null;
     }
 
+    /**
+     * Instantiates a new Prepare shipment response.
+     *
+     * @param item              the item
+     * @param packaging         the packaging
+     * @param fulfillmentCenter the fulfillment center
+     */
     public PrepareShipmentResponse(Item item, Packaging packaging, FulfillmentCenter fulfillmentCenter) {
         this.item = item;
         this.packaging = packaging;
@@ -56,8 +66,8 @@ public class PrepareShipmentResponse {
             return false;
         }
         PrepareShipmentResponse that = (PrepareShipmentResponse) o;
-        return getItem().equals(that.getItem()) && getPackaging().equals(that.getPackaging())
-                                                && getFulfillmentCenter().equals(that.getFulfillmentCenter());
+        return getItem().equals(that.getItem()) && getPackaging().equals(that.getPackaging()) &&
+            getFulfillmentCenter().equals(that.getFulfillmentCenter());
     }
 
     @Override
