77a27016264694026820171a730dee6df9a61714
Will Reynolds <billiamreynolds@gmail.com>
2021-12-09 20:21:49 -0600

    codegrade-push-3

--- src/com/amazon/ata/cost/WeightedCostStrategy.java
+++ src/com/amazon/ata/cost/WeightedCostStrategy.java
@@ -1,18 +1,18 @@
 package com.amazon.ata.cost;
 
-import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
-
 import com.amazon.ata.types.Material;
 import com.amazon.ata.types.ShipmentCost;
 import com.amazon.ata.types.ShipmentOption;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
 public class WeightedCostStrategy implements CostStrategy {
-    
+
     private Map<BigDecimal, CostStrategy> map;
 
-    
+
     /**
      * Instantiates a new Weighted cost strategy.
      *
@@ -22,48 +22,49 @@ public class WeightedCostStrategy implements CostStrategy {
         this.map = new HashMap<>();
         this.map.putAll(map);
     }
-    
+
     /**
      * Instantiates a new Weighted cost strategy.
      */
     public WeightedCostStrategy() {
-    
+
     }
-    
+
     private ShipmentCost getShipmentCostMon(CostStrategy cs, ShipmentOption shipmentOption, BigDecimal scale) {
         return new ShipmentCost(shipmentOption, cs.getCost(shipmentOption).getCost().multiply(scale)
                 .add(BigDecimal.valueOf(2.04)));
     }
+
     private ShipmentCost getShipmentCostMon2(CostStrategy cs, ShipmentOption shipmentOption, BigDecimal scale) {
         return new ShipmentCost(shipmentOption, cs.getCost(shipmentOption).getCost().multiply(scale)
                 .add(BigDecimal.valueOf(0.1296)));
     }
-    
+
     @Override
     public ShipmentCost getCost(ShipmentOption shipmentOption) {
         for (Map.Entry<BigDecimal, CostStrategy> entry : map.entrySet()) {
             Material material = entry.getValue().getCost(shipmentOption)
                     .getShipmentOption().getPackaging().getMaterial();
-            
+
             CostStrategy cs = entry.getValue();
             BigDecimal scale = entry.getKey();
             boolean isOfTypeMonetaryCost = cs instanceof MonetaryCostStrategy;
-            
+
             if (material == Material.CORRUGATE) {
                 if (isOfTypeMonetaryCost) {
                     return getShipmentCostMon(cs, shipmentOption, scale);
                 }
                 return getShipmentCostMon(cs, shipmentOption, scale);
             }
-            
+
             if (isOfTypeMonetaryCost) {
                 return getShipmentCostMon2(cs, shipmentOption, scale);
             }
-            
+
             break;
         }
-        
+
         return getShipmentCostMon2(this, shipmentOption, BigDecimal.valueOf(0.08));
     }
-    
+
 }
