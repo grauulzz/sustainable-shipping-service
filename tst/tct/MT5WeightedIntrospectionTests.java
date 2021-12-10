ea8e3f862d1c4a4bedd3ff22353e47eec42f7a34
Will Reynolds <billiamreynolds@gmail.com>
2021-12-09 22:59:35 -0600

    0.75 coverage
     Results: SUCCESS (137 tests, 137 successes, 0 failures, 0 skipped)

--- tst/tct/MT5WeightedIntrospectionTests.java
+++ tst/tct/MT5WeightedIntrospectionTests.java
@@ -1,8 +1,11 @@
 package tct;
 
+import com.amazon.ata.cost.*;
 import com.amazon.ata.test.reflect.MethodInvoker;
 import com.amazon.ata.test.reflect.MethodQuery;
 
+import com.amazon.ata.types.*;
+import org.junit.jupiter.api.*;
 import tct.basewrappers.BoxWrapper;
 import tct.basewrappers.CarbonCostStrategyWrapper;
 import tct.basewrappers.MonetaryCostStrategyWrapper;
@@ -10,10 +13,6 @@ import tct.basewrappers.PolyBagWrapper;
 import tct.basewrappers.ShipmentCostWrapper;
 import tct.basewrappers.ShipmentOptionWrapper;
 import tct.basewrappers.WeightedCostStrategyWrapper;
-import com.amazon.ata.types.PackagingFactory;
-import com.amazon.ata.types.ShipmentOptionFactory;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -115,4 +114,163 @@ public class MT5WeightedIntrospectionTests {
             fail("Expected a 'costStrategy' field in the ShipmentService class. Has it been modified?");
         }
     }
+
+    public static class WeightedCostStrategyTest {
+
+        private static final Packaging BOX_10x10x20 =
+                new Box(Material.CORRUGATE, BigDecimal.valueOf(10),
+                        BigDecimal.valueOf(10), BigDecimal.valueOf(20));
+
+        private static final Packaging BoxLaminatedPlastic_10x10x20 =
+                new Box(Material.LAMINATED_PLASTIC, BigDecimal.valueOf(10),
+                        BigDecimal.valueOf(10), BigDecimal.valueOf(20));
+
+        private static final Packaging POLYBAG_10X10X20 =
+                new PolyBag(Material.LAMINATED_PLASTIC, BigDecimal.valueOf(10),
+                        BigDecimal.valueOf(10), BigDecimal.valueOf(20));
+
+        private static final Packaging PolyBagCorrugate_10x10x20 =
+                new PolyBag(Material.CORRUGATE, BigDecimal.valueOf(10),
+                        BigDecimal.valueOf(10), BigDecimal.valueOf(20));
+
+        private CostStrategy strategy;
+
+        @BeforeEach
+        void setUp() {
+            strategy = new WeightedCostStrategy();
+        }
+
+        @Test
+        void laminatedPlastic_getCarbonCost_returnsCost() {
+            // GIVEN
+            ShipmentOption laminatedPlastic = ShipmentOption.builder()
+                    .withPackaging(POLYBAG_10X10X20)
+                    .build();
+
+            // WHEN
+            strategy = new CarbonCostStrategy();
+            ShipmentCost shipmentCost = strategy.getCost(laminatedPlastic);
+
+            // THEN 0.2592 is %80 of the cost strategy being called
+            assertEquals(0,
+                    BigDecimal.valueOf(0.3240).compareTo(shipmentCost.getCost()),
+                    "Incorrect carbon cost calculation for a polybag with a volume of 20000cc.");
+        }
+
+
+        @Test
+        void corrugate_getCarbonCost_returnsCost() {
+            // GIVEN
+            ShipmentOption corrugate = ShipmentOption.builder()
+                    .withPackaging(BOX_10x10x20)
+                    .build();
+
+            // WHEN
+            strategy = new CarbonCostStrategy();
+            ShipmentCost shipmentCost = strategy.getCost(corrugate);
+
+            // THEN
+            assertEquals(0,
+                    BigDecimal.valueOf(17.00).compareTo(shipmentCost.getCost()),
+                    "Incorrect carbon cost calculation for a box with dimensions 10x10x20.");
+        }
+
+        @Test
+        void corrugate_getMonetaryCost_returnsCost() {
+            // GIVEN
+            ShipmentOption option = ShipmentOption.builder()
+                    .withPackaging(BOX_10x10x20)
+                    .build();
+
+            // WHEN
+            strategy = new MonetaryCostStrategy();
+            ShipmentCost shipmentCost = strategy.getCost(option);
+
+            // THEN
+            assertEquals(0, BigDecimal.valueOf(5.43).compareTo(shipmentCost.getCost()),
+                    "Incorrect monetary cost calculation for a box with dimensions 10x10x20.");
+        }
+
+        @Test
+        void laminatedPlastic_getMonetaryCost_returnsCost() {
+            // GIVEN
+            ShipmentOption option = ShipmentOption.builder()
+                    .withPackaging(BOX_10x10x20)
+                    .build();
+
+            // WHEN
+            strategy = new MonetaryCostStrategy();
+            ShipmentCost shipmentCost = strategy.getCost(option);
+
+            // THEN
+            assertEquals(0, BigDecimal.valueOf(5.43).compareTo(shipmentCost.getCost()),
+                    "Incorrect monetary cost calculation for a box with dimensions 10x10x20.");
+        }
+
+        @Test
+        void polybagCorrugateCombo_getMonetaryCost_returnsCost() {
+            // GIVEN
+            ShipmentOption option = ShipmentOption.builder()
+                    .withPackaging(PolyBagCorrugate_10x10x20)
+                    .build();
+
+            // WHEN material and packaging are swapped
+            strategy = new MonetaryCostStrategy();
+            ShipmentCost shipmentCost = strategy.getCost(option);
+
+            // THEN cost should be very cheap (PolyBag, Corrugate)
+            assertEquals(BigDecimal.valueOf(0.5650).compareTo(shipmentCost.getCost()), 0,
+                    "Incorrect monetary cost calculation for a polybag of dimensions 10x10x20");
+        }
+
+        @Test
+        void boxLaminatedPlastic_getMonetaryCost_returnsCost() {
+            // GIVEN
+            ShipmentOption option = ShipmentOption.builder()
+                    .withPackaging(BoxLaminatedPlastic_10x10x20)
+                    .build();
+
+            // WHEN material and packaging are swapped
+            strategy = new MonetaryCostStrategy();
+            ShipmentCost shipmentCost = strategy.getCost(option);
+
+            // THEN Price should be very expensive (Box, LaminatedPlastic)
+            assertEquals(BigDecimal.valueOf(250.43).compareTo(shipmentCost.getCost()), 0,
+                    "Incorrect monetary cost calculation for a polybag of dimensions 10x10x20");
+        }
+        @Test
+        void polybagCorrugateCombo_getCarbonCost_returnsCost() {
+            // GIVEN
+            ShipmentOption option = ShipmentOption.builder()
+                    .withPackaging(PolyBagCorrugate_10x10x20)
+                    .build();
+
+            // WHEN material and packaging are swapped
+            strategy = new CarbonCostStrategy();
+            ShipmentCost shipmentCost = strategy.getCost(option);
+
+            // THEN cost should be very cheap (PolyBag, Corrugate)
+            assertEquals(BigDecimal.valueOf(0.4590).compareTo(shipmentCost.getCost()), 0,
+                    "Incorrect monetary cost calculation for a polybag of dimensions 10x10x20");
+        }
+
+        @Test
+        void boxLaminatedPlastic_getCarbonCost_returnsCost() {
+            // GIVEN
+            ShipmentOption option = ShipmentOption.builder()
+                    .withPackaging(BoxLaminatedPlastic_10x10x20)
+                    .build();
+
+            // WHEN material and packaging are swapped
+            strategy = new CarbonCostStrategy();
+            ShipmentCost shipmentCost = strategy.getCost(option);
+
+            // THEN Price should be very expensive (Box, LaminatedPlastic)
+            assertEquals(BigDecimal.valueOf(12.00).compareTo(shipmentCost.getCost()), 0,
+                    "Incorrect monetary cost calculation for a polybag of dimensions 10x10x20");
+        }
+
+    }
+
+
 }
